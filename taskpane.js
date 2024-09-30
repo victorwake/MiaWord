fc3f0ae701004be6a95594be731cdcd19891eff7
victor pinto <victorpintowake@gmail.com>
2024-09-30 20:45:10 -0300

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -1,3 +1,118 @@
-/*! For license information please see taskpane.js.LICENSE.txt */
-!function(){var t={98362:function(t,e,r){"use strict";t.exports=r.p+"assets/logo-filled.png"},58394:function(t,e,r){"use strict";t.exports=r.p+"054888a74685b7c90cb6.css"}},e={};function r(n){var o=e[n];if(void 0!==o)return o.exports;var i=e[n]={exports:{}};return t[n](i,i.exports,r),i.exports}r.m=t,r.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(t){if("object"==typeof window)return window}}(),r.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},function(){var t;r.g.importScripts&&(t=r.g.location+"");var e=r.g.document;if(!t&&e&&(e.currentScript&&"SCRIPT"===e.currentScript.tagName.toUpperCase()&&(t=e.currentScript.src),!t)){var n=e.getElementsByTagName("script");if(n.length)for(var o=n.length-1;o>-1&&(!t||!/^http(s?):/.test(t));)t=n[o--].src}if(!t)throw new Error("Automatic publicPath is not supported in this browser");t=t.replace(/#.*$/,"").replace(/\?.*$/,"").replace(/\/[^\/]+$/,"/"),r.p=t}(),r.b=document.baseURI||self.location.href,function(){function t(e){return t="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(t){return typeof t}:function(t){return t&&"function"==typeof Symbol&&t.constructor===Symbol&&t!==Symbol.prototype?"symbol":typeof t},t(e)}function e(){"use strict";e=function(){return n};var r,n={},o=Object.prototype,i=o.hasOwnProperty,a=Object.defineProperty||function(t,e,r){t[e]=r.value},c="function"==typeof Symbol?Symbol:{},u=c.iterator||"@@iterator",s=c.asyncIterator||"@@asyncIterator",l=c.toStringTag||"@@toStringTag";function f(t,e,r){return Object.defineProperty(t,e,{value:r,enumerable:!0,configurable:!0,writable:!0}),t[e]}try{f({},"")}catch(r){f=function(t,e,r){return t[e]=r}}function h(t,e,r,n){var o=e&&e.prototype instanceof w?e:w,i=Object.create(o.prototype),c=new _(n||[]);return a(i,"_invoke",{value:j(t,r,c)}),i}function p(t,e,r){try{return{type:"normal",arg:t.call(e,r)}}catch(t){return{type:"throw",arg:t}}}n.wrap=h;var y="suspendedStart",d="suspendedYield",m="executing",v="completed",g={};function w(){}function b(){}function x(){}var E={};f(E,u,(function(){return this}));var L=Object.getPrototypeOf,T=L&&L(L(B([])));T&&T!==o&&i.call(T,u)&&(E=T);var k=x.prototype=w.prototype=Object.create(E);function S(t){["next","throw","return"].forEach((function(e){f(t,e,(function(t){return this._invoke(e,t)}))}))}function O(e,r){function n(o,a,c,u){var s=p(e[o],e,a);if("throw"!==s.type){var l=s.arg,f=l.value;return f&&"object"==t(f)&&i.call(f,"__await")?r.resolve(f.__await).then((function(t){n("next",t,c,u)}),(function(t){n("throw",t,c,u)})):r.resolve(f).then((function(t){l.value=t,c(l)}),(function(t){return n("throw",t,c,u)}))}u(s.arg)}var o;a(this,"_invoke",{value:function(t,e){function i(){return new r((function(r,o){n(t,e,r,o)}))}return o=o?o.then(i,i):i()}})}function j(t,e,n){var o=y;return function(i,a){if(o===m)throw Error("Generator is already running");if(o===v){if("throw"===i)throw a;return{value:r,done:!0}}for(n.method=i,n.arg=a;;){var c=n.delegate;if(c){var u=I(c,n);if(u){if(u===g)continue;return u}}if("next"===n.method)n.sent=n._sent=n.arg;else if("throw"===n.method){if(o===y)throw o=v,n.arg;n.dispatchException(n.arg)}else"return"===n.method&&n.abrupt("return",n.arg);o=m;var s=p(t,e,n);if("normal"===s.type){if(o=n.done?v:d,s.arg===g)continue;return{value:s.arg,done:n.done}}"throw"===s.type&&(o=v,n.method="throw",n.arg=s.arg)}}}function I(t,e){var n=e.method,o=t.iterator[n];if(o===r)return e.delegate=null,"throw"===n&&t.iterator.return&&(e.method="return",e.arg=r,I(t,e),"throw"===e.method)||"return"!==n&&(e.method="throw",e.arg=new TypeError("The iterator does not provide a '"+n+"' method")),g;var i=p(o,t.iterator,e.arg);if("throw"===i.type)return e.method="throw",e.arg=i.arg,e.delegate=null,g;var a=i.arg;return a?a.done?(e[t.resultName]=a.value,e.next=t.nextLoc,"return"!==e.method&&(e.method="next",e.arg=r),e.delegate=null,g):a:(e.method="throw",e.arg=new TypeError("iterator result is not an object"),e.delegate=null,g)}function N(t){var e={tryLoc:t[0]};1 in t&&(e.catchLoc=t[1]),2 in t&&(e.finallyLoc=t[2],e.afterLoc=t[3]),this.tryEntries.push(e)}function P(t){var e=t.completion||{};e.type="normal",delete e.arg,t.completion=e}function _(t){this.tryEntries=[{tryLoc:"root"}],t.forEach(N,this),this.reset(!0)}function B(e){if(e||""===e){var n=e[u];if(n)return n.call(e);if("function"==typeof e.next)return e;if(!isNaN(e.length)){var o=-1,a=function t(){for(;++o<e.length;)if(i.call(e,o))return t.value=e[o],t.done=!1,t;return t.value=r,t.done=!0,t};return a.next=a}}throw new TypeError(t(e)+" is not iterable")}return b.prototype=x,a(k,"constructor",{value:x,configurable:!0}),a(x,"constructor",{value:b,configurable:!0}),b.displayName=f(x,l,"GeneratorFunction"),n.isGeneratorFunction=function(t){var e="function"==typeof t&&t.constructor;return!!e&&(e===b||"GeneratorFunction"===(e.displayName||e.name))},n.mark=function(t){return Object.setPrototypeOf?Object.setPrototypeOf(t,x):(t.__proto__=x,f(t,l,"GeneratorFunction")),t.prototype=Object.create(k),t},n.awrap=function(t){return{__await:t}},S(O.prototype),f(O.prototype,s,(function(){return this})),n.AsyncIterator=O,n.async=function(t,e,r,o,i){void 0===i&&(i=Promise);var a=new O(h(t,e,r,o),i);return n.isGeneratorFunction(e)?a:a.next().then((function(t){return t.done?t.value:a.next()}))},S(k),f(k,l,"Generator"),f(k,u,(function(){return this})),f(k,"toString",(function(){return"[object Generator]"})),n.keys=function(t){var e=Object(t),r=[];for(var n in e)r.push(n);return r.reverse(),function t(){for(;r.length;){var n=r.pop();if(n in e)return t.value=n,t.done=!1,t}return t.done=!0,t}},n.values=B,_.prototype={constructor:_,reset:function(t){if(this.prev=0,this.next=0,this.sent=this._sent=r,this.done=!1,this.delegate=null,this.method="next",this.arg=r,this.tryEntries.forEach(P),!t)for(var e in this)"t"===e.charAt(0)&&i.call(this,e)&&!isNaN(+e.slice(1))&&(this[e]=r)},stop:function(){this.done=!0;var t=this.tryEntries[0].completion;if("throw"===t.type)throw t.arg;return this.rval},dispatchException:function(t){if(this.done)throw t;var e=this;function n(n,o){return c.type="throw",c.arg=t,e.next=n,o&&(e.method="next",e.arg=r),!!o}for(var o=this.tryEntries.length-1;o>=0;--o){var a=this.tryEntries[o],c=a.completion;if("root"===a.tryLoc)return n("end");if(a.tryLoc<=this.prev){var u=i.call(a,"catchLoc"),s=i.call(a,"finallyLoc");if(u&&s){if(this.prev<a.catchLoc)return n(a.catchLoc,!0);if(this.prev<a.finallyLoc)return n(a.finallyLoc)}else if(u){if(this.prev<a.catchLoc)return n(a.catchLoc,!0)}else{if(!s)throw Error("try statement without catch or finally");if(this.prev<a.finallyLoc)return n(a.finallyLoc)}}}},abrupt:function(t,e){for(var r=this.tryEntries.length-1;r>=0;--r){var n=this.tryEntries[r];if(n.tryLoc<=this.prev&&i.call(n,"finallyLoc")&&this.prev<n.finallyLoc){var o=n;break}}o&&("break"===t||"continue"===t)&&o.tryLoc<=e&&e<=o.finallyLoc&&(o=null);var a=o?o.completion:{};return a.type=t,a.arg=e,o?(this.method="next",this.next=o.finallyLoc,g):this.complete(a)},complete:function(t,e){if("throw"===t.type)throw t.arg;return"break"===t.type||"continue"===t.type?this.next=t.arg:"return"===t.type?(this.rval=this.arg=t.arg,this.method="return",this.next="end"):"normal"===t.type&&e&&(this.next=e),g},finish:function(t){for(var e=this.tryEntries.length-1;e>=0;--e){var r=this.tryEntries[e];if(r.finallyLoc===t)return this.complete(r.completion,r.afterLoc),P(r),g}},catch:function(t){for(var e=this.tryEntries.length-1;e>=0;--e){var r=this.tryEntries[e];if(r.tryLoc===t){var n=r.completion;if("throw"===n.type){var o=n.arg;P(r)}return o}}throw Error("illegal catch attempt")},delegateYield:function(t,e,n){return this.delegate={iterator:B(t),resultName:e,nextLoc:n},"next"===this.method&&(this.arg=r),g}},n}function r(t,e,r,n,o,i,a){try{var c=t[i](a),u=c.value}catch(t){return void r(t)}c.done?e(u):Promise.resolve(u).then(n,o)}function n(t){return function(){var e=this,n=arguments;return new Promise((function(o,i){var a=t.apply(e,n);function c(t){r(a,o,i,c,u,"next",t)}function u(t){r(a,o,i,c,u,"throw",t)}c(void 0)}))}}function o(){return i.apply(this,arguments)}function i(){return i=n(e().mark((function t(){return e().wrap((function(t){for(;;)switch(t.prev=t.next){case 0:return t.abrupt("return",Word.run(function(){var t=n(e().mark((function t(r){var n,o,i;return e().wrap((function(t){for(;;)switch(t.prev=t.next){case 0:return(n=r.document.getSelection()).load("text"),t.next=4,r.sync();case 4:o=n.text,i=localStorage.getItem("authToken"),o?a(i,o):document.getElementById("response-container").innerText="No text selected.";case 7:case"end":return t.stop()}}),t)})));return function(e){return t.apply(this,arguments)}}()));case 1:case"end":return t.stop()}}),t)}))),i.apply(this,arguments)}function a(t,e){console.log("Enviando authToken:",t),$.ajax({url:"https://servidor-complemento.onrender.com/api/text",type:"POST",contentType:"application/json",data:JSON.stringify({text:e,authToken:t}),success:function(t){s(t)},error:function(t,e,r){var n;401===t.status?(n=t.responseJSON.message,c()):n="Error del servidor.",s({message:n})}})}function c(){document.getElementById("auth-dialog").style.display="block"}function u(){document.getElementById("auth-dialog").style.display="none",document.getElementById("response-container").innerText=""}function s(t){var e=document.getElementById("response-container");t&&t.message?e.innerText=t.message:e.innerText="No response from server."}Office.onReady((function(t){t.host===Office.HostType.Word&&(document.getElementById("app-body").style.display="flex",document.getElementById("run").onclick=o)})),document.getElementById("submitAuthToken").onclick=function(){var t=document.getElementById("authTokenInput").value.trim(),e=document.getElementById("error-message");if(0===t.length)return e.innerText="Por favor, ingresa un token válido.",void(e.style.display="block");localStorage.setItem("authToken",t),function(t){$.ajax({url:"https://servidor-complemento.onrender.com/api/authtoken",type:"POST",contentType:"application/json",data:JSON.stringify({authToken:t}),success:function(t){s(t)},error:function(t,e,r){var n,o=document.getElementById("error-message");401===t.status?(n=t.responseJSON.message,c()):n="Error del servidor.",o.innerText=n,o.style.display="block"}})}(t),u()},document.getElementById("cancelAuthToken").onclick=u}(),function(){"use strict";new URL(r(58394),r.b),new URL(r(98362),r.b)}()}();
-//# sourceMappingURL=taskpane.js.map
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office, Word */
+/* global localStorage console $ */
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Word) {
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+async function run() {
+  return Word.run(async (context) => {
+    const selection = context.document.getSelection();
+    selection.load("text");
+
+    await context.sync();
+
+    const selectedText = selection.text;
+    const authToken = localStorage.getItem("authToken");
+    // const authToken = "1234"
+
+    if (selectedText) {
+      sendTextToServer(authToken, selectedText);
+    } else {
+      document.getElementById("response-container").innerText = "No text selected.";
+    }
+  });
+}
+
+function sendTextToServer(authToken, text) {
+  console.log("Enviando authToken:", authToken);
+  $("#loadingMessage").show();
+  $.ajax({
+    url: "https://servidor-complemento.onrender.com/api/text",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({ text: text, authToken: authToken }),
+    success: function (response) {
+      $("#loadingMessage").hide();
+      displayResponse(response);
+    },
+    error: function (xhr, status, error) {
+      let errorMessage;
+      if (xhr.status === 401) {
+        errorMessage = xhr.responseJSON.message;
+        showAuthTokenDialog();
+      } else {
+        errorMessage = "Error del servidor.";
+      }
+      displayResponse({ message: errorMessage });
+    },
+  });
+}
+
+function sendAuthToken(authToken) {
+  $.ajax({
+    url: "https://servidor-complemento.onrender.com/api/authtoken",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({ authToken: authToken }),
+    success: function (response) {
+      displayResponse(response);
+    },
+    error: function (xhr, status, error) {
+      const errorContainer = document.getElementById("error-message");
+      let errorMessage;
+
+      if (xhr.status === 401) {
+        errorMessage = xhr.responseJSON.message;
+        showAuthTokenDialog();
+      } else {
+        errorMessage = "Error del servidor.";
+      }
+
+      errorContainer.innerText = errorMessage;
+      errorContainer.style.display = "block";
+    },
+  });
+}
+
+function showAuthTokenDialog() {
+  document.getElementById("auth-dialog").style.display = "block";
+}
+document.getElementById("submitAuthToken").onclick = handleSubmitAuthToken;
+
+function hideAuthTokenDialog() {
+  document.getElementById("auth-dialog").style.display = "none";
+  document.getElementById("response-container").innerText = "";
+}
+document.getElementById("cancelAuthToken").onclick = hideAuthTokenDialog;
+
+function handleSubmitAuthToken() {
+  const authToken = document.getElementById("authTokenInput").value.trim();
+  const errorContainer = document.getElementById("error-message");
+
+  if (authToken.length === 0) {
+    errorContainer.innerText = "Por favor, ingresa un token válido.";
+    errorContainer.style.display = "block";
+    return;
+  }
+
+  localStorage.setItem("authToken", authToken);
+  sendAuthToken(authToken);
+  hideAuthTokenDialog();
+}
+
+function displayResponse(response) {
+  const responseContainer = document.getElementById("response-container");
+  if (response && response.message) {
+    responseContainer.innerText = response.message;
+  } else {
+    responseContainer.innerText = "No response from server.";
+  }
+}
